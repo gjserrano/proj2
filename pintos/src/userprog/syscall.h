66ccbeb8630069281a5495ef4b37f328e65a8779
Blake Holman <baholman@ksu.edu>
2019-03-07 12:32:02 -0600

    Syscalls complete

--- pintos/src/userprog/syscall.h
+++ pintos/src/userprog/syscall.h
@@ -1,11 +1,22 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
+#include "threads/synch.h"
+
+#define CLOSE_ALL -1
+#define ERROR -1
+
+#define NOT_LOADED 0
+#define LOAD_SUCCESS 1
+#define LOAD_FAIL 2
 
 struct child_process {
 	int pid;
+	int load;
 	bool wait;
 	bool exit;
 	int status;
+	struct lock wait_lock;
+	struct list_elem elem;
 };
 
 void syscall_init (void);
@@ -23,6 +34,17 @@ int syscall_write(int fd, const void *buffer, unsigned size);
 void syscall_seek(int fd, unsigned position);
 unsigned syscall_tell(int fd);
 void syscall_close(int fd);
+//extra functions
+struct child_process* get_child_process(int pid);
+int process_add_file(struct file *f);
+struct file* process_get_file(int fd);
+void process_close_file(int fd);
+void get_arg(struct intr_frame *f, int *arg, int n);
+void check_valid_ptr(const void *vaddr);
+void check_valid_buffer(void* buffer, unsigned size);
+void remove_child_process(struct child_process *cp);
+void remove_child_processes(void);
+int user_to_kernel_ptr(const void *vaddr);
 
 
 #endif /* userprog/syscall.h */
